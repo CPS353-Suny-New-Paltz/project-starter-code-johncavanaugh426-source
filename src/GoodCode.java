38b1e2f2855d72436742a3f1975934d7309c1b10
johncavanaugh426-source <john.cavanaugh426@gmail.com>
2025-08-27 19:37:50 -0400

    a
    a

---
+++ src/GoodCode.java
@@ -0,0 +1,6 @@
+public class GoodCode{
+public static void main(String[] args){
+System.out.println("This code works but is badly formatted");
+int a=5;int b=10;int sum=a+b;System.out.println("Sum is "+sum);
+}
+}
