7144ea0efafca889e0eb58f6e68131d7f3384ea3
johncavanaugh426-source <john.cavanaugh426@gmail.com>
2025-08-28 13:19:29 -0400

    Fix failing code for status check test

--- src/FailingTest.java
+++ src/FailingTest.java
@@ -1,6 +1,7 @@
 // src/FailingTest.java
 public class FailingTest {
     public static void main(String[] args) {
-        System.out.println("Hello)  // Syntax error, missing closing quote
-    } // This also fails checkstyle: improper indentation
+        System.out.println("Hello");
+    }
 }
+
