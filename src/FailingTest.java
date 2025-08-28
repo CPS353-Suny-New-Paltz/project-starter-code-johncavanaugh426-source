ba704782b1c2f9d0004b39f5f519d4e8c307a556
johncavanaugh426-source <john.cavanaugh426@gmail.com>
2025-08-28 13:15:34 -0400

    Add intentionally failing code for status check test

---
+++ src/FailingTest.java
@@ -0,0 +1,6 @@
+// src/FailingTest.java
+public class FailingTest {
+    public static void main(String[] args) {
+        System.out.println("Hello)  // Syntax error, missing closing quote
+    } // This also fails checkstyle: improper indentation
+}
