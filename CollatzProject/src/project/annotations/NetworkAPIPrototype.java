b04f618db2644ebbbebebf4cc4e9b7dd33c1e950
johncavanaugh426-source <john.cavanaugh426@gmail.com>
2025-09-08 16:25:57 -0400

    Update NetworkAPIPrototype.java
    had to remove the import with a * cause that breaks checkstyle :(

--- CollatzProject/src/project/annotations/NetworkAPIPrototype.java
+++ CollatzProject/src/project/annotations/NetworkAPIPrototype.java
@@ -1,7 +1,12 @@
 package project.annotations;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
 
 @Retention(RetentionPolicy.RUNTIME) 
 @Target(ElementType.METHOD)         
 public @interface NetworkAPIPrototype { }
+
