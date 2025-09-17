0a9118d5f5bc472f2474a54c07ea69edb4719494
johncavanaugh426-source <john.cavanaugh426@gmail.com>
2025-09-17 14:25:58 -0400

    Smoke tests part of checkpoint 3 (#16)
    Created the smoke tests for each api

---
+++ test/project/tests/ComputeEngineAPITest.java
@@ -0,0 +1,30 @@
+package project.tests;
+
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import project.api.conceptual.ComputeEngineAPI;
+import project.api.conceptual.ComputeRequest;
+import project.api.conceptual.ComputeResult;
+
+public class ComputeEngineAPITest {
+
+    @Test
+    public void smokeTestComputeEngine() {
+        ComputeEngineAPI mockEngine = mock(ComputeEngineAPI.class);
+        ComputeRequest mockRequest = new ComputeRequest() {
+            @Override
+            public int getInputNumber() {
+                return 6;
+            }
+        };
+
+        ComputeResult expectedResult = new ComputeResult(true, "mock result");
+        when(mockEngine.computeCollatz(mockRequest)).thenReturn(expectedResult);
+        ComputeResult result = mockEngine.computeCollatz(mockRequest);
+        assert(result.isSuccess());
+        assert(result.getSequence().equals("mock result"));
+        verify(mockEngine).computeCollatz(mockRequest);
+    }
+}
---
+++ test/project/tests/UserComputeAPITest.java
@@ -0,0 +1,43 @@
+package project.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import project.api.network.UserComputeAPI;
+import project.api.network.UserComputeRequest;
+import project.api.network.UserComputeResult;
+
+public class UserComputeAPITest {
+
+    @Test
+    public void smokeTestUserComputeAPI() {
+        UserComputeAPI mockAPI = Mockito.mock(UserComputeAPI.class);
+
+        UserComputeRequest mockRequest = new UserComputeRequest() {
+            @Override
+            public String getInputSource() {
+                return "input_data.txt";
+            }
+
+            @Override
+            public String getOutputDelimiter() {
+                return null;
+            }
+
+            @Override
+            public String getOutputDestination() {
+                return "output_data.txt";
+            }
+        };
+
+        UserComputeResult expectedResult = new UserComputeResult(true, "mock success");
+        when(mockAPI.processInput(mockRequest)).thenReturn(expectedResult);
+        UserComputeResult result = mockAPI.processInput(mockRequest);
+        Assertions.assertTrue(result.isSuccess());
+        Assertions.assertEquals("mock success", result.getMessage());
+        verify(mockAPI).processInput(mockRequest);
+    }
+}
