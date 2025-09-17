c86f8ed70468c407402227a568934a47c3d40e34
johncavanaugh426-source <john.cavanaugh426@gmail.com>
2025-09-17 13:37:27 -0400

    Create TestTest.java

---
+++ test/testtest/TestTest.java
@@ -0,0 +1,12 @@
+package testtest;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestTest {
+
+    @Test
+    public void basicJUnitWorks() {
+        assertTrue(true, "JUnit is working!");
+    }
+}
