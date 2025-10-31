7c9e20c7ff5903493ca364834e25b26eeff7992a
johncavanaugh426-source <john.cavanaugh426@gmail.com>
2025-10-30 22:06:09 -0400

    Set up a gRPC server for the network API layer (#42)
    this took 5 hours

---
+++ src/project/grpc/UserComputeClient.java
@@ -0,0 +1,14 @@
+package project.grpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
+import java.util.Scanner;
+
+public class UserComputeClient {
+
+    public static void main(String[] args) {
+        System.out.println("UserComputeClient started...");
+    }
+}
