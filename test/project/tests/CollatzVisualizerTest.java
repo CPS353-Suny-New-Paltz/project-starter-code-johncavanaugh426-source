48082059c287d57a8d7a08d89b32e85c6de4b9fb
johncavanaugh426-source <john.cavanaugh426@gmail.com>
2025-11-24 13:25:34 -0500

    Added a Visualizer to see collatz sequences (#52)
    * Added a Visualizer to see collatz sequences
    
    I have it set to not run for extremely large numbers cause the output will be really ugly
    
    * package name

---
+++ test/project/tests/CollatzVisualizerTest.java
@@ -0,0 +1,26 @@
+package project.tests;
+
+import org.junit.jupiter.api.Test;
+
+import project.visualizer.CollatzVisualizerFromFile;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CollatzVisualizerTest {
+
+    @Test
+    public void testVisualizerReadsOutputFile() throws Exception {
+        // Make sure the input file exists
+        File inputFile = new File("chartInput.txt");
+        assertTrue(inputFile.exists(), "Input file chartInput.txt should exist");
+
+        // Run the visualizer 
+        CollatzVisualizerFromFile.main(new String[]{});
+
+        // check that output file is created
+        File outputFile = new File("collatzCharts.txt");
+        assertTrue(outputFile.exists(), "Visualizer should create collatzCharts.txt");
+    }
+}
